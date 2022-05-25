db5aaf644d0024ff10beb67d90eecf5babdd4495
ASIR2SIGB <106262244+ASIR2SIGB@users.noreply.github.com>
2022-05-25 20:49:12 +0200

    Delete update.ps1

--- update.ps1
+++
@@ -1,6 +0,0 @@
-$content = ( Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ASIR2SIGB/SIGB/main/SIGB.ps1" -UseBasicParsing | Select-Object content); 
-$content.content | Out-File "$ENV:USERPROFILE\APPDATA\Roaming\SIGB\SIGB.ps1" -Verbose:$true
-Write-Host "INICIA EL ACCESO DIRECTO LLAMADO 'SIGB' UBICADO EN SU ESCRITORIO" -ForegroundColor green
-
-
-Start-Sleep -Seconds 90
